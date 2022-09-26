23b6ff2bdc98dceb3ed7f7ac8bd4670ebb1fc576
Igor Torres <igor.s.torres@outlook.com>
2022-09-26 19:54:05 -0300

    Interfaces dos Repositorios

---
+++ CleanArchitectureMVC/src/CleanArchitectureMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitectureMVC.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureMVC.Domain.Interfaces
+{
+    public interface IProductRepository : IAbstractRepository<Product>
+    {
+        Task<Product> GetProductCategoryAsync(int id);
+    }
+}
