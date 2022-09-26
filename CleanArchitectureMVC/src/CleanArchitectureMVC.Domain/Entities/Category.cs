4a473718f6dfc844418593f7e3f3646f0c599369
Igor Torres <igor.s.torres@outlook.com>
2022-09-26 19:07:45 -0300

    Enriquecendo o modelo de domínio - Entidade Category

--- CleanArchitectureMVC/src/CleanArchitectureMVC.Domain/Entities/Category.cs
+++ CleanArchitectureMVC/src/CleanArchitectureMVC.Domain/Entities/Category.cs
@@ -1,12 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchitectureMVC.Domain.Validation;
+using System.Collections.Generic;
 
 namespace CleanArchitectureMVC.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
 
-        public ICollection<Product> Products { get; set; }
+        public Category(string name)
+        {
+            ValidadeDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            ValidadeDomain(name, id);
+        }
+        
+        public void Update(string name)
+        {
+            ValidadeDomain(name);
+        }
+        private void ValidadeDomain(string name, int? id = null)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name) || name.Length < 3, "Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimun 3 characters");
+            
+            Name = name;
+
+            if(id != null)
+            {
+                DomainExceptionValidation.When(id.Value <= 0, "Invalid Id value");
+                Id = id.Value;
+            }
+        }
+
     }
 }
---
+++ CleanArchitectureMVC/src/CleanArchitectureMVC.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CleanArchitectureMVC.Domain.Validation
+{
+    public class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string message) : base(message){}
+
+        public static void When(bool hasError, string error)
+        {
+            if(hasError)
+                throw new DomainExceptionValidation(error);
+        }
+    }
+}
