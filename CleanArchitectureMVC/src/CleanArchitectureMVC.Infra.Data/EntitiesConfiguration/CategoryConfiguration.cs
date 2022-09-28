756b8f19bc0a93a60538f19a00bf96c18f8762c7
Igor Torres <igor.s.torres@outlook.com>
2022-09-28 12:46:38 -0300

    Implementando o DbContext e o FluentAPI

---
+++ CleanArchitectureMVC/src/CleanArchitectureMVC.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitectureMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitectureMVC.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+                );
+        }
+    }
+}
