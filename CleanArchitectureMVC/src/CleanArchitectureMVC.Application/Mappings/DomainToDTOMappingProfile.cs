a1e755904353c940cb8f9e3370647c2bb27d303d
Igor Torres <igor.s.torres@outlook.com>
2022-09-30 14:46:06 -0300

    Implementando os DTOs e o Automapper

---
+++ CleanArchitectureMVC/src/CleanArchitectureMVC.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitectureMVC.Application.DTO;
+using CleanArchitectureMVC.Domain.Entities;
+
+namespace CleanArchitectureMVC.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
